9145a0d7bfab017ce14380f23b771e2642881611
Hwaiteu <benner.andrey.loon@gmail.com>
2026-07-17 18:29:55 +0300

    fix: plugin name

--- server/src/register.ts
+++ server/src/register.ts
@@ -4,7 +4,7 @@ const register = ({ strapi }: { strapi: Core.Strapi }) => {
   strapi.customFields.register({
     type: 'json',
     name: 'strapi-table',
-    plugin: 'strapi-table',
+    plugin: '@mish.dev/strapi-table',
   });
 };
 
